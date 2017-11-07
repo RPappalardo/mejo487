d90f4938a980951aef9d1faaa9ff328e47f5df4b
Rachel Pappalardo <rachelan@live.unc.edu>
2017-11-07 00:54:06 -0500

    Commit

--- project3/js/scripts.js
+++ project3/js/scripts.js
@@ -5,3 +5,46 @@ $('#example').dataTable( {
 
 });
 });
+var r = new Array(), j = -1;
+for (var key=0, size=data.length; key<size; key++){
+    r[++j] ='<tr><td>';
+    r[++j] = data[key][0];
+    r[++j] = '</td><td class="whatever1">';
+    r[++j] = data[key][1];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][2];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][3];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][4];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][5];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][6];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][7];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][8];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][9];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][10];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][11];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][12];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][13];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][14];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][15];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][16];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][17];
+    r[++j] = '</td><td class="whatever2">';
+    r[++j] = data[key][18];
+    r[++j] = '</td></tr>';
+}
+$('#example').html(r.join(''));
