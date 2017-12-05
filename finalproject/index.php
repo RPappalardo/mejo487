bd581512bf4019a8c1924466eefb6e2c4dc04f00
Rachel Pappalardo <rachelan@live.unc.edu>
2017-12-05 00:15:16 -0500

    test

--- finalproject/index.php
+++ finalproject/index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
   <link type="text/css" rel="stylesheet" href="/finalproject/css/styles.css">
   <link rel="stylesheet" href="finalproject/tweetmap/tweetMap-1.1.0.min.css">
 </head>
